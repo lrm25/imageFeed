7675c5d614be29153ab04700f86fd549974a1edf
Luke McCrone <luke.r.mccrone@gmail.com>
2020-07-10 00:31:17 -0600

    pulling top image page

--- imagefeed/imagefeed/spiders/space_porn_spider.py
+++ imagefeed/imagefeed/spiders/space_porn_spider.py
@@ -4,10 +4,19 @@ class SpacePornSpider(scrapy.Spider):
 
     name = "space-porn"
 
+    reddit_main_page = "https://www.reddit.com"
+    space_porn_main_page = reddit_main_page + "/r/spaceporn"
+
     def start_requests(self):
-        urls = ["https://www.reddit.com/r/spaceporn"]
+        urls = [self.space_porn_main_page]
         for url in urls:
             yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        print(response.css('a[data-click-id=body]').getall())
+        if response.url == self.space_porn_main_page:
+            yield scrapy.Request(self.reddit_main_page + response.css('a[data-click-id=body]::attr(href)').get(),
+            callback=self.parse_image_page)
+
+    def parse_image_page(self, response):
+        print(response.text)
+
