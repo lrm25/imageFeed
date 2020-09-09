6caf187e0f6e06c70098c244a8eae9158f8c5bf3
Luke McCrone <luke.r.mccrone@gmail.com>
2020-09-08 23:30:06 -0600

    initial unittest stub

---
+++ imagefeed/imagefeed/tests/test_space_porn.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestSpacePorn(unittest.TestCase):
+
+    def test_nonexistent_url(self):
+        print("Initial stub")
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
